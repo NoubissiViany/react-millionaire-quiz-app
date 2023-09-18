3815c88b77c3b618668ba064c7a6c2116eb57053
NNVG <g696607013@gmail.com>
2023-09-18 11:09:27 +0100

    Done with the design of the reward section

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,43 @@
 import React from "react";
+import { useState } from "react";
 import "./app.css";
 
 export default function App() {
+  const [questionNum, setQuestionNum] = useState(1);
   const rewardsAmount = [
-    { id: 1, amount: "100 FCFA" },
-    { id: 2, amount: "200 FCFA" },
-    { id: 3, amount: "300 FCFA" },
-    { id: 4, amount: "500 FCFA" },
-    { id: 5, amount: "1000 FCFA" },
-    { id: 6, amount: "2000 FCFA" },
-    { id: 7, amount: "4000 FCFA" },
-    { id: 8, amount: "8000 FCFA" },
-    { id: 9, amount: "16000 FCFA" },
-    { id: 10, amount: "32000 FCFA" },
-    { id: 11, amount: "64000 FCFA" },
-    { id: 12, amount: "125000 FCFA" },
-    { id: 13, amount: "25000 FCFA" },
-    { id: 14, amount: "500000 FCFA" },
-    { id: 15, amount: "1000000 FCFA" },
+    { id: 1, amount: "100" },
+    { id: 2, amount: "200" },
+    { id: 3, amount: "300" },
+    { id: 4, amount: "500" },
+    { id: 5, amount: "1000" },
+    { id: 6, amount: "2000" },
+    { id: 7, amount: "4000" },
+    { id: 8, amount: "8000" },
+    { id: 9, amount: "16000" },
+    { id: 10, amount: "32000" },
+    { id: 11, amount: "64000" },
+    { id: 12, amount: "125000" },
+    { id: 13, amount: "25000" },
+    { id: 14, amount: "500000" },
+    { id: 15, amount: "1000000" },
   ].reverse();
   return (
     <div className="appConatainer">
-      <div className="main">main</div>
+      <div className="main">
+        <div className="top">
+          <div className="timer">30</div>
+        </div>
+        <div className="bottom">question and answers</div>
+      </div>
       <div className="rewards">
         <ul className="rewardList">
           {rewardsAmount.map((m) => (
-            <li key={m.id} className="rewardListItem">
+            <li
+              key={m.id}
+              className={
+                questionNum === m.id ? "rewardListItem active" : "rewardListItem"
+              }
+            >
               <span className="rewardListItemNumber">{m.id}</span>
               <span className="rewardListItemAmount">{m.amount}</span>
             </li>
